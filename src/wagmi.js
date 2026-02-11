fe8e8a2f57bd082851f07a95070d2232e264fe7e
Chayut Orapinpatipat <chayut_o@hotmail.com>
2026-02-11 17:02:31 +1100

    Update wagmi.js

--- src/wagmi.js
+++ src/wagmi.js
@@ -4,7 +4,7 @@ import { http } from "wagmi";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "SaThuCoin",
-  projectId: "sathucoin-web",
+  projectId: "5e58e29133e55977b5760f8af4775cb4",
   chains: [base],
   transports: {
     [base.id]: http("https://mainnet.base.org"),
